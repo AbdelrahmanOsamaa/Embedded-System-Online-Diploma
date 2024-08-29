026996ec7a60f39d6ad14beddf5a9430086fceb5
AbdelrahmanOsamaa <abdelrahman.osama336@gmail.com>
2024-08-29 17:18:04 +0300

    revision

---
+++ Embedded C/Platfrom types/Platform_types.h
@@ -0,0 +1,25 @@
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
+typedef bool boolean;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64; 
+
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
+typedef signed long long sint64;
+
+typedef volatile unsigned char vuint8;
+typedef volatile unsigned short vuint16;
+typedef volatile unsigned int vuint32;
+typedef volatile unsigned long long vuint64;
+
+typedef volatile signed char vsint8;
+typedef volatile signed short vsint16;
+typedef volatile signed int vsint32;
+typedef volatile signed long long vsint64;
+
+#endif
